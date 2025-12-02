78a8d860287d2af479aa9462e372a24231c382c6
Kris1990_PL <MRK1990@wp.pl>
2025-12-02 20:42:37 +0100

    add casual object with football players

--- dist/index.js
+++ dist/index.js
@@ -21,6 +21,49 @@ const students = {
     eric: { age: 20, major: "Psychology", gpa: 3.6 },
     fiona: { age: 19, major: "Literature", gpa: 3.4 },
 };
+// Example for football players
+const footballPlayers = {
+    messi: {
+        goals: 807,
+        assists: 350,
+        redCards: 2,
+        yellowCards: 90,
+        hatTricks: 56,
+        freeKickGoals: 65,
+    },
+    ronaldo: {
+        goals: 873,
+        assists: 236,
+        redCards: 11,
+        yellowCards: 130,
+        hatTricks: 63,
+        freeKickGoals: 60,
+    },
+    lewandowski: {
+        goals: 625,
+        assists: 150,
+        redCards: 0,
+        yellowCards: 40,
+        hatTricks: 30,
+        freeKickGoals: 10,
+    },
+    zidane: {
+        goals: 125,
+        assists: 160,
+        redCards: 14,
+        yellowCards: 120,
+        hatTricks: 1,
+        freeKickGoals: 15,
+    },
+    ronaldinho: {
+        goals: 280,
+        assists: 180,
+        redCards: 3,
+        yellowCards: 70,
+        hatTricks: 4,
+        freeKickGoals: 66,
+    },
+};
 function mapDict(obj, cb) {
     const result = {};
     for (const key in obj) {
@@ -47,13 +90,32 @@ function reduceDict(obj, cb, initial) {
 }
 // COMMANDS
 // BLACK WEEK DISCOUNT CARS
-console.log(mapDict(cars, (k, v) => ({ brand: v.brand, color: v.color, price: v.price * 0.5 })));
+console.log(mapDict(cars, (k, v) => ({
+    brand: v.brand,
+    color: v.color,
+    price: v.price * 0.5,
+})));
 //SEARCH IT NERDS STUDENTS
 console.log(filterDict(students, (k, v) => v.major === "Computer Science" || v.major === "Mathematics"));
 // SEARCH COMMON BRAND CARS
 console.log(filterDict(cars, (k, v) => v.brand === "Tesla"));
 // TOTAL VALUE ALL CARS
 console.log(reduceDict(cars, (acc, k, v) => acc + v.price, 0));
+// BEST OF ALL TIME
+// instruction
+// goal - 3 pst
+// assists - 1.5 pts
+// redCard - -30 pts
+// yellow - -10 pts
+// hatTricks - 21 pts
+// freeKickGoal - 10 pts
+console.log(mapDict(footballPlayers, (k, v) => v.goals * 3 +
+    v.assists * 1.5 +
+    v.hatTricks * 21 +
+    v.freeKickGoals * 10 +
+    (v.redCards * -30 + v.redCards * -10)));
+//AVERAGE GOALS PER PLAYERS
+console.log(reduceDict(footballPlayers, (acc, k, v) => acc + v.goals, 0) / Object.keys(footballPlayers).length);
 // Array.prototype.map, but for Dict
 // function mapDict(...args: any[]): any {}
 // Array.prototype.filter, but for Dict
--- src/index.ts
+++ src/index.ts
@@ -23,47 +23,139 @@ const students = {
   fiona: { age: 19, major: "Literature", gpa: 3.4 },
 };
 
+// Example for football players
+const footballPlayers = {
+  messi: {
+    goals: 807,
+    assists: 350,
+    redCards: 2,
+    yellowCards: 90,
+    hatTricks: 56,
+    freeKickGoals: 65,
+  },
+
+  ronaldo: {
+    goals: 873,
+    assists: 236,
+    redCards: 11,
+    yellowCards: 130,
+    hatTricks: 63,
+    freeKickGoals: 60,
+  },
+
+  lewandowski: {
+    goals: 625,
+    assists: 150,
+    redCards: 0,
+    yellowCards: 40,
+    hatTricks: 30,
+    freeKickGoals: 10,
+  },
+
+  zidane: {
+    goals: 125,
+    assists: 160,
+    redCards: 14,
+    yellowCards: 120,
+    hatTricks: 1,
+    freeKickGoals: 15,
+  },
+
+  ronaldinho: {
+    goals: 280,
+    assists: 180,
+    redCards: 3,
+    yellowCards: 70,
+    hatTricks: 4,
+    freeKickGoals: 66,
+  },
+};
+
 interface Dict<T> {
   [k: string]: T;
 }
 
-function mapDict<T,U>(obj: Dict<T>,cb: (key: string,value: T) => U): Dict<U> {
-    const result: Dict<U> = {}
-    for (const key in obj) {
-        result[key] = cb(key,obj[key])
-    }
-    return result
+function mapDict<T, U>(
+  obj: Dict<T>,
+  cb: (key: string, value: T) => U
+): Dict<U> {
+  const result: Dict<U> = {};
+  for (const key in obj) {
+    result[key] = cb(key, obj[key]);
+  }
+  return result;
 }
 
-function filterDict<T>(obj: Dict<T>,cb: (key: string,value: T) => boolean): Dict<T> {
-    const result: Dict<T> = {}
-    for (const key in obj) {
-        const value = cb(key,obj[key])
-        if (value) {
-            result[key] = obj[key]
-        }
+function filterDict<T>(
+  obj: Dict<T>,
+  cb: (key: string, value: T) => boolean
+): Dict<T> {
+  const result: Dict<T> = {};
+  for (const key in obj) {
+    const value = cb(key, obj[key]);
+    if (value) {
+      result[key] = obj[key];
     }
-    return result
+  }
+  return result;
 }
 
-function reduceDict<T,U>(obj: Dict<T>,cb: (acc: U,key: string,value: T) => U,initial: U): U {
-    let result: U = initial
-    for (const key in obj) {
-        result = cb(result,key,obj[key])
-    }
-    return result
+function reduceDict<T, U>(
+  obj: Dict<T>,
+  cb: (acc: U, key: string, value: T) => U,
+  initial: U
+): U {
+  let result: U = initial;
+  for (const key in obj) {
+    result = cb(result, key, obj[key]);
+  }
+  return result;
 }
 
 // COMMANDS
 
 // BLACK WEEK DISCOUNT CARS
-console.log(mapDict(cars,(k,v) => ({brand: v.brand, color: v.color, price: v.price*0.5})))
+console.log(
+  mapDict(cars, (k, v) => ({
+    brand: v.brand,
+    color: v.color,
+    price: v.price * 0.5,
+  }))
+);
 //SEARCH IT NERDS STUDENTS
-console.log(filterDict(students,(k,v) => v.major === "Computer Science" || v.major === "Mathematics"))
+console.log(
+  filterDict(
+    students,
+    (k, v) => v.major === "Computer Science" || v.major === "Mathematics"
+  )
+);
 // SEARCH COMMON BRAND CARS
-console.log(filterDict(cars,(k,v) => v.brand === "Tesla"))
+console.log(filterDict(cars, (k, v) => v.brand === "Tesla"));
 // TOTAL VALUE ALL CARS
-console.log(reduceDict(cars,(acc,k,v) => acc + v.price,0))
+console.log(reduceDict(cars, (acc, k, v) => acc + v.price, 0));
+// BEST OF ALL TIME
+// instruction
+
+// goal - 3 pst
+// assists - 1.5 pts
+// redCard - -30 pts
+// yellow - -10 pts
+// hatTricks - 21 pts
+// freeKickGoal - 10 pts
+
+console.log(
+  mapDict(
+    footballPlayers,
+    (k, v) =>
+      v.goals * 3 +
+      v.assists * 1.5 +
+      v.hatTricks * 21 +
+      v.freeKickGoals * 10 +
+      (v.redCards * -30 + v.redCards * -10)
+  )
+);
+//AVERAGE GOALS PER PLAYERS
+console.log(reduceDict(footballPlayers,(acc,k,v) => acc + v.goals,0)/Object.keys(footballPlayers).length)
 
 // Array.prototype.map, but for Dict
 // function mapDict(...args: any[]): any {}
